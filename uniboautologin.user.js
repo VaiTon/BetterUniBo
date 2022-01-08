b264220e7148b1fdf9566ec8d93f63d99d744420
VaiTon <eyadlorenzo@gmail.com>
2022-01-08 23:46:13 +0100

    added sol style and almaesami style

--- uniboautologin.user.js
+++ uniboautologin.user.js
@@ -5,38 +5,70 @@
 // @author       VaiTon <eyadlorenzo@gmail.com>
 // @match        https://idp.unibo.it/adfs/ls/*
 // @match        https://shib.unibo.it/idp/*
+// @match        https://almaesami.unibo.it/*
+// @match        https://studenti.unibo.it/*
 // @icon         https://www.google.com/s2/favicons?domain=unibo.it
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  'use strict';
-
-  if (window.HRD) {
-    // Select UniBo
-    window.HRD.selection('AD AUTHORITY');
-    return;
+  function injectCSS(style) {
+    let el = document.createElement('style');
+    el.innerHTML = style;
+    document.getElementsByTagName('head')[0].appendChild(el);
   }
 
-  let loginForm = document.forms.loginForm;
-  if (loginForm && window.PasswordCredential) {
-    navigator.credentials.get({ password: true }).then((cred) => {
-      // Login through the browser identity manager
-      loginForm.elements.userNameInput.value = cred.id;
-      loginForm.elements.passwordInput.value = cred.password;
-      loginForm.submit();
-    });
-    return;
-  }
+  ('use strict');
 
-  if (document.location.origin === 'https://shib.unibo.it') {
+  if (document.location.origin === 'https://idp.unibo.it') {
+    if (window.HRD) {
+      // Select UniBo
+      window.HRD.selection('AD AUTHORITY');
+      return;
+    }
+
+    let loginForm = document.forms.loginForm;
+    if (loginForm && window.PasswordCredential) {
+      navigator.credentials.get({ password: true }).then((cred) => {
+        // Login through the browser identity manager
+        loginForm.elements.userNameInput.value = cred.id;
+        loginForm.elements.passwordInput.value = cred.password;
+        loginForm.submit();
+      });
+      return;
+    }
+  } else if (document.location.origin === 'https://shib.unibo.it') {
     let loginForm = document.forms[0];
 
     navigator.credentials.get({ password: true }).then((cred) => {
       loginForm.elements.j_username.value = cred.id;
       loginForm.elements.j_password.value = cred.password;
-      loginForm.elements._eventId_proceed.click()
+      loginForm.elements._eventId_proceed.click();
     });
     return;
+  } else if (document.location.origin === 'https://almaesami.unibo.it') {
+    injectCSS(`
+      .titoloPagina {
+        color: initial;
+        font-family: "Roboto", sans-serif;
+        font-size: 1.5rem;
+        font-weight: bold;
+        margin-left: 1rem;
+      }
+      span.sottotitoloPagina {
+        margin-left: 1rem;
+      }
+    `);
+  } else if (document.location.origin === 'https://studenti.unibo.it') {
+    injectCSS(`
+      .box .services div, .box ul li {
+        border-radius: 5px;
+        box-shadow: none;
+      }
+      .box .services div:hover, .box ul li:hover {
+            background: #00000010;
+
+      }
+    `);
   }
 })();
